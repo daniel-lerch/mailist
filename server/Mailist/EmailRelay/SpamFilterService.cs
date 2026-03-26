a32726a2d22cf925a9c9bdbb066f6e19df96ffbb
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2026-03-26 19:22:21 +0100

    Extensions AI boilerplate

---
+++ server/Mailist/EmailRelay/SpamFilterService.cs
@@ -0,0 +1,27 @@
+﻿using Mailist.EmailRelay.Entities;
+using Microsoft.Extensions.AI;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mailist.EmailRelay;
+
+public class SpamFilterService
+{
+    private readonly IChatClient chatClient;
+
+    public async Task ClassifyMessage(InboxEmail email, CancellationToken cancellationToken)
+    {
+        ChatOptions chatOptions = new()
+        {
+
+        };
+
+        List<ChatMessage> messages = [
+            new(ChatRole.System, ""),
+            new(ChatRole.User, "")
+        ];
+
+        var response = await chatClient.GetResponseAsync(messages, chatOptions, cancellationToken);
+    }
+}
