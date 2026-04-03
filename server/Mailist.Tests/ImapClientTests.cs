fd0518b9c1990f3e25e4a2e57eaa679316f84f48
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2026-04-03 19:19:32 +0200

    Improve unit tests for IdleFetchAsync

--- server/Mailist.Tests/ImapClientTests.cs
+++ server/Mailist.Tests/ImapClientTests.cs
@@ -55,7 +55,8 @@ public class ImapClientTests
         var cancel = new CancellationTokenSource();
         var client = new Mock<IImapClient>();
         var folder = new Mock<IMailFolder>();
-        var newMessage = new Mock<IMessageSummary>();
+        var firstMessage = new Mock<IMessageSummary>();
+        var secondMessage = new Mock<IMessageSummary>();
         client.Setup(c => c.Capabilities).Returns(ImapCapabilities.Idle);
         client.Setup(c => c.IdleAsync(It.IsAny<CancellationToken>(), It.IsAny<CancellationToken>()))
             .Returns(async (CancellationToken doneToken, CancellationToken cancellationToken) =>
@@ -71,20 +72,32 @@ public class ImapClientTests
             });
         folder.Setup(f => f.IsOpen).Returns(true);
         folder.Setup(f => f.Count).Returns(0);
+        firstMessage.Setup(m => m.UniqueId).Returns(new UniqueId(1));
+        secondMessage.Setup(m => m.UniqueId).Returns(new UniqueId(2));
 
         IAsyncEnumerator<IMessageSummary> enumerator = client.Object
             .IdleFetchAsync(folder.Object, MessageSummaryItems.UniqueId, cancel.Token)
             .GetAsyncEnumerator(CancellationToken.None);
 
-        ValueTask<bool> task = enumerator.MoveNextAsync();
+        ValueTask<bool> task1 = enumerator.MoveNextAsync();
         await Task.Delay(100, TestContext.Current.CancellationToken);
-        folder.Setup(f => f.FetchAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<IFetchRequest>(), It.IsAny<CancellationToken>()).Result)
-            .Returns([newMessage.Object]);
+        folder.Setup(f => f.FetchAsync(0, 0, It.IsAny<IFetchRequest>(), It.IsAny<CancellationToken>()).Result)
+            .Returns([firstMessage.Object]);
         folder.Setup(f => f.Count).Returns(1);
         folder.Raise(f => f.CountChanged += null, EventArgs.Empty);
 
-        Assert.True(await task);
-        Assert.Same(newMessage.Object, enumerator.Current);
+        Assert.True(await task1);
+        Assert.Same(firstMessage.Object, enumerator.Current);
+
+        ValueTask<bool> task2 = enumerator.MoveNextAsync();
+        await Task.Delay(100, TestContext.Current.CancellationToken);
+        folder.Setup(f => f.FetchAsync(1, 1, It.IsAny<IFetchRequest>(), It.IsAny<CancellationToken>()).Result)
+            .Returns([secondMessage.Object]);
+        folder.Setup(f => f.Count).Returns(2);
+        folder.Raise(f => f.CountChanged += null, EventArgs.Empty);
+
+        Assert.True(await task2);
+        Assert.Same(secondMessage.Object, enumerator.Current);
 
         cancel.CancelAfter(100);
         await Assert.ThrowsAnyAsync<OperationCanceledException>(() => enumerator.MoveNextAsync().AsTask());
